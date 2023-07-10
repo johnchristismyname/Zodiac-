4d5d75c52993569672b11f1936231a0068e749bd
johnchristismyname <139159120+johnchristismyname@users.noreply.github.com>
2023-07-11 03:20:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+document.querySelector("h6").addEventListener("click", () => {
+    window.scrollTo(0, 0);
+});
+
+document.querySelector("#a").addEventListener("click", () => {
+    window.scrollTo(0, 950);
+})
+
+document.querySelector("#b").addEventListener("click", () => {
+    window.scrollTo(0, 1850);
+})
+
+document.querySelector("#c").addEventListener("click", () => {
+    window.scrollTo(0, 2750);
+})
+
+document.querySelector("#d").addEventListener("click", () => {
+    window.scrollTo(0, 3650);
+})
+
+document.querySelector("#e").addEventListener("click", () => {
+    window.scrollTo(0, 4550);
+})
+
+document.querySelector("#f").addEventListener("click", () => {
+    window.scrollTo(0, 5450);
+})
+
+document.querySelector("#g").addEventListener("click", () => {
+    window.scrollTo(0, 6350);
+})
+
+document.querySelector("#h").addEventListener("click", () => {
+    window.scrollTo(0, 7250);
+})
+
+document.querySelector("#i").addEventListener("click", () => {
+    window.scrollTo(0, 8150);
+})
+
+document.querySelector("#j").addEventListener("click", () => {
+    window.scrollTo(0, 9050);
+})
+
+document.querySelector("#k").addEventListener("click", () => {
+    window.scrollTo(0, 9950);
+})
+
+document.querySelector("#l").addEventListener("click", () => {
+    window.scrollTo(0, 10850);
+})
